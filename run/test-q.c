d7b772c12b6dff307e5a8cdfa1f2a17385a3b308
andrewpatri <andrea.patri@me.com>
2026-04-21 18:51:38 +0200

    modified karman.c to have also a q field that changes in time, various tests were done and also output

--- run/test-q.c
+++ run/test-q.c
@@ -1,39 +1,143 @@
-// this file is a test for the declaration in a point of q_source
+/**
+# Bénard–von Kármán Vortex Street for flow around a cylinder at Re=160
 
-// #include "embed.h"
+An example of 2D viscous flow around a simple solid boundary. Fluid is
+injected to the left of a channel bounded by solid walls with a slip
+boundary condition. A passive tracer is injected in the bottom half of
+the inlet.
 
+![Animation of the vorticity field.](karman/vort.mp4)(loop)
 
-scalar q[];
+![Animation of the tracer field.](karman/f.mp4)(loop)
 
+We use the centered Navier-Stokes solver, with embedded boundaries and
+advect the passive tracer *f*. */
 
-int maxlevel = 7; int minlevel = 2;
-double D0 = 3.e-2;
-double tend = 10.; 
+#include "embed.h"
+#include "navier-stokes/centered.h"
+// #include "navier-stokes/perfs.h"
+#include "tracer.h"
 
-int main(){
+scalar f[];
+scalar * tracers = {f};
+double Reynolds = 160.;
+int maxlevel = 9;
+face vector muv[];
 
-    // q changes iteratively with time
-    q[top] = 10. * pow(t, 1.5);
+scalar qsorg[];
+/**
+The domain is eight units long, centered vertically. */
 
-    // square domain 
-    L0 = D0 *3;
-
-    origin(-L0/2, 0);
-
-    run();
-
+int main()
+{
+  L0 = 8. [1];
+  origin (-0.5, -L0/2.);
+  N = 512;
+  mu = muv;
+  
+  /**
+  When using bview we can interactively control the Reynolds number
+  and maximum level of refinement. */
+  
+  display_control (Reynolds, 10, 1000);
+  display_control (maxlevel, 6, 12);
+  
+  run(); 
 }
 
-//define rectangle
-event init(i=0){
-    q[top] = 10.;
+/**
+We set a constant viscosity based on the Reynolds number, the cylinder
+diameter $D$ and the inflow velocity $U0$. */
 
-    //solid    
+double D = 0.125, U0 = 1.;
+
+event properties (i++)
+{
+  foreach_face()
+    muv.x[] = fm.x[]*D*U0/Reynolds;
 }
 
-event output( t =+ 1) {
+/**
+The fluid is injected on the left boundary with velocity $U0$. The
+tracer is injected in the lower-half of the left boundary. An outflow
+condition is used on the right boundary. */
 
-    fprintf (stderr, "%g/n, t");
+u.n[left]  = dirichlet(U0);
+p[left]    = neumann(0.);
+pf[left]   = neumann(0.);
+f[left]    = dirichlet(y < 0);
+
+u.n[right] = neumann(0.);
+p[right]   = dirichlet(0.);
+pf[right]  = dirichlet(0.);
+
+/**
+The cylinder is no-slip. */
+
+u.n[embed] = dirichlet(0.);
+u.t[embed] = dirichlet(0.);
+
+event init (t = 0)
+{
+
+  /**
+  The domain contains a cylinder of diameter 0.125. */
+
+  solid (cs, fs, sqrt(sq(x) + sq(y)) - D/2.);
+  foreach(){
+  if (x>0)
+  qsorg[] = 10.;
+  }
+  /**
+  We set the initial velocity field. */
+  
+  foreach()
+    u.x[] = cs[] ? U0 : 0.;
+}
+event update_q (i++){
+  foreach(){
+  if (x>0)
+  qsorg[] = 10.*t;
+  else
+  qsorg[] = 10.;
+  }
+
+}
+/**
+We check the number of iterations of the Poisson and viscous
+problems. */
+
+event logfile (i++)
+  // fprintf (stderr, "%d %g %d %d\n", i, t, mgp.i, mgu.i);
+    fprintf (stderr, "%g\n", t);
+/**
+We produce animations of the vorticity and tracer fields... */
+
+event movies (i += 4; t <= 15.)
+{
+  scalar omega[], m[];
+  vorticity (u, omega);
+  foreach()
+    m[] = cs[] - 0.5;
+  output_ppm (omega, file = "vort.mp4", box = {{-0.5,-0.5},{7.5,0.5}},
+	      min = -10, max = 10, linear = true, mask = m);
+  //output_ppm (f, file = "f.mp4", box = {{-0.5,-0.5},{7.5,0.5}},
+	     // linear = false, min = 0, max = 1, mask = m);
+  output_ppm (qsorg, file = "q.mp4" , min = 10, max = 100, linear = true);
 }
 
-event stop ( t = tend)
+
+
+/**
+We adapt according to the error on the embedded geometry, velocity and
+tracer fields. */
+
+event adapt (i++) {
+  adapt_wavelet ({cs,u,f}, {1e-2,3e-2,3e-2,3e-2}, maxlevel, 4);
+}
+
+/**
+## See also
+
+* [Same example with Gerris](https://gerris.dalembert.upmc.fr/gerris/examples/examples/cylinder.html)
+*/
