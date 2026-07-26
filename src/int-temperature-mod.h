cd930cd28510e7660fc86877a7964be87d23e7a3
andrewpatri <andrea.patri@me.com>
2026-07-26 14:56:53 +0200

    typo

--- src/int-temperature-mod.h
+++ src/int-temperature-mod.h
@@ -95,7 +95,7 @@ int EqTemperature (const gsl_vector * xdata, void * params, gsl_vector * fdata) 
   double lambda1vh = n.x / (n.x + n.y) * lambda1v.x[] + n.y / (n.x + n.y) * lambda1v.y[];
   double lambda2vh = n.x / (n.x + n.y) * lambda2v.x[] + n.y / (n.x + n.y) * lambda2v.y[];
    #if QSOURCE
- if (data->c.y <  Y_descriminante) {
+ if (data->c.y <  Y_discriminante) {
     gsl_vector_set(fdata, 0,
                  //-divq_rad_int(TInti, RADIATION_TEMP, data->emissivity)
                  + lambda1vh * gradTSn 
